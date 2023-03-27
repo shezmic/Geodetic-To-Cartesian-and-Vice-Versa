8fc4d019bcea4d1def8aecac1e18eb8a9233e520
Micaiah Muuo Wambua <mcwambua@gmail.com>
2023-03-27 09:35:20 +0300

    Update to make it more efficient
    I made the following changes to improve the code:
    
    1. Removed unnecessary comments and print statements: I removed comments that were either redundant or not necessary for understanding the code. I also removed extra print statements to make the output cleaner.
    
    2. Return values instead of printing: In the `cartesian_to_geodetic` and `geodetic_to_cartesian` functions, I changed the code to return the values instead of printing them. This makes the functions more reusable and modular.
    
    3. Use f-strings for formatting: I replaced the old string formatting method with f-strings, which are more readable and concise.
    
    4. Added float conversion for user input: I added float conversion to the user input to ensure the values are correctly interpreted as numbers.
    
    5. Encapsulated code in a main function: I moved the main logic of the code into a `main()` function. This makes the code more organized and easier to understand.
    
    6. Added `if __name__ == "__main__":` statement: This statement ensures that the main function is only executed when the script is run directly, not when it is imported as a module.

--- converter.py
+++ converter.py
@@ -2,59 +2,57 @@
  * @author [shezmic]
  * @email [mcwambua@gmail.com]
  * @create date 2019-03-05 02:39:08
- * @modify date 2019-03-05 02:39:08
+ * @modify date 2023-03-27 09:34:08
  * @desc [description]
  */
 
+    
 import pyproj
 
-# Definition of the function to convert Cartesian coordinates to Geodetic
+# Function to convert Cartesian coordinates to Geodetic
 def cartesian_to_geodetic(x, y, z):
+    # Define the coordinate systems
     ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
     lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
-
+    # Perform the coordinate transformation
     lon, lat, alt = pyproj.transform(ecef, lla, x, y, z, radians=False)
+    return lat, lon, alt
 
-    print("\n")
-    print ("Latitude: %f" % lat)
-    print ("Longitude: %f" % lon)
-    print ("Height in metres: %f" % alt)
-
-
-# Definition of the function to convert Geodetic coordinates to Cartesian
+# Function to convert Geodetic coordinates to Cartesian
 def geodetic_to_cartesian(lat, lon, alt):
+    # Define the coordinate systems
     ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
     lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
-
+    # Perform the coordinate transformation
     x, y, z = pyproj.transform(lla, ecef, lon, lat, alt, radians=False)
+    return x, y, z
 
-    print("\n")
-    print ("X: %f" % x)
-    print ("Y: %f" % y)
-    print ("Z: %f" % z)
+def main():
+    # Get user's choice of conversion
+    choice = input("Enter A (Cartesian to Geodetic) or B (Geodetic to Cartesian)\n")
 
-# Brings up a prompt, basically just to make it easier for the user to enter the values
-choice = input("Enter A (Cartesian to Geodetic) or B (Geodetic to Cartesian)" + "\r\n")
+    if choice == 'A':
+        # Get Cartesian coordinates from user
+        x = float(input("Enter X coordinate: "))
+        y = float(input("Enter Y coordinate: "))
+        z = float(input("Enter Z coordinate: "))
+        # Perform conversion and display results
+        lat, lon, alt = cartesian_to_geodetic(x, y, z)
+        print(f"\nLatitude: {lat}\nLongitude: {lon}\nHeight in metres: {alt}")
 
-# Self explanatory
-if choice == 'A':
-    print("You chose Cartesian to Geodetic." + "\r\n")
-    print("Enter these values: " + "\n")
-    x = input("Enter X coordinate:  ")
-    y = input("Enter Y coordinate:  ")
-    z = input("Enter Z coordinate:  ")
-    cartesian_to_geodetic(x, y, z)
+    elif choice == 'B':
+        # Get Geodetic coordinates from user
+        lat = float(input("Enter Latitude: "))
+        lon = float(input("Enter Longitude: "))
+        alt = float(input("Enter Height in metres: "))
+        # Perform conversion and display results
+        x, y, z = geodetic_to_cartesian(lat, lon, alt)
+        print(f"\nX: {x}\nY: {y}\nZ: {z}")
 
-# Self Explanatory too
-elif choice == 'B':
-    print("You chose Geodetic to Cartesian." + "\r\n")
-    print("Enter these values: " + "\n")
-    lat = input("Enter Latitude:  ")
-    lon = input("Enter Longitude:  ")
-    alt = input("Enter Height in metres:  ")
-    geodetic_to_cartesian(lat, lon, alt)
+    else:
+        print("Sorry, wrong selection...")
+        return
 
-# Unless someone is trying to play chess or something else other than do their homework ...lol
-else:
-    print("Sorry wrong selection...")
-    exit
+# Run the main function when the script is executed
+if __name__ == "__main__":
+    main()
